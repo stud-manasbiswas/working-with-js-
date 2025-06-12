1352323aafb9ee3d746c1fbea5cc0959c6e569bb
stud-manasbiswas <manasbiswas342@gmail.com>
2025-06-12 20:43:33 +0530

    done

---
+++ lec 2 variables/index.js
@@ -0,0 +1,39 @@
+// variable = a container that stores a value .
+
+// behaves as if it were the value it contains
+
+// 1.declaration     let x;
+// assignment        x=100;
+
+let x;
+x=100;
+
+console.log(x);
+
+let age = 25;
+let price=10.99;
+let gpa = 8.9
+console.log(typeof age)
+console.log(`You are ${age} years old`)
+console.log(`the price is $${price} `)
+console.log(`your gpa  is ${gpa} `)
+
+// strings 
+
+let firstname ="manas";
+console.log(typeof firstname)
+console.log(`Your Nmae is ${firstname}`)
+
+// booleans 
+
+let online = true; // can be true or false 
+console.log(typeof online)
+
+let name = "Manas Biswas"
+let agee = 25;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `your name is ${name}`;
+document.getElementById("p2").textContent = `You are ${age} years old`
+document.getElementById("p3").textContent = `Enrooled ${isStudent}`
+
--- lec1 introduction/index.js
+++ lec1 introduction/index.js
@@ -1,6 +1,11 @@
 console.log(`hello`)
 console.log(`i like pizza`)
 
+// selecting elements by id 
+
+document.getElementById("myH1").textContent = "hello"
+document.getElementById("myP").textContent = "I like Pizza "
+
 // window.alert(`This ia an alert`)
 
 // this is a comments --> not displayed as output
@@ -11,5 +16,3 @@ is a
 multiline comment 
 */
 
-document.getElementById("myH1").textContent = "hello"
-document.getElementById("myP").textContent = "I like Pizza "
