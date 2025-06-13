653f6aa71b5a6871612d495260a1196f76bf6dd5
stud-manasbiswas <manasbiswas342@gmail.com>
2025-06-13 23:02:31 +0530

    done

---
+++ lec 5 conditional statements/index.js
@@ -0,0 +1,22 @@
+console.log(" conditional statements")
+
+// IF STATEMENTS = if a condition is true, execute some code
+// if not , do else part
+
+let age = 25;
+let haslicence = false;
+if(age >= 18){
+    console.log("you are old enough to driving compettition");
+    if(haslicence){
+        console.log("you have the licenece")
+    }
+    else{
+        console.log("you do not have the licence")
+    }
+}
+else{
+    console.log("you are not eligible")
+}
+
+
+
