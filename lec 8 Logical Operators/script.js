01c92b65bd17a15daba791efba9687cb1a247860
stud-manasbiswas <manasbiswas342@gmail.com>
2025-06-14 21:03:37 +0530

    done

---
+++ lec 8 Logical Operators/script.js
@@ -0,0 +1,16 @@
+// Logical Operators
+
+
+// AND = &&
+// OR = ||
+// Not = !
+
+const temp = 200;
+
+if(temp > 0 && temp<= 30){
+    console.log("The Weather is GOOD");
+}
+
+else{
+    console.log("The Weather is BAD")
+}
